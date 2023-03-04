255b97ca2b685799a628919687ddbf3708e022f8
Keitoto <keitasekihara@gmail.com>
2023-03-04 21:13:50 +0900

    ♻️ Moved providers to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import { AppShell, Container } from '@mantine/core';
-import { Route, Routes } from 'react-router-dom';
+import {
+  AppShell,
+  ColorScheme,
+  ColorSchemeProvider,
+  Container,
+  MantineProvider,
+} from '@mantine/core';
+import { useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import { HeaderSimple } from '@/components/Header';
 import { AboutPage } from '@/pages/About';
 import { ContactPage } from '@/pages/Contact';
 import { ErrorPage } from '@/pages/Error';
 import { HomePage } from '@/pages/Home';
+import { UserDetailPage } from '@/pages/UserDetail';
 import { UserListPage } from '@/pages/UserList';
 
 const navLinks = [
@@ -16,20 +24,37 @@ const navLinks = [
 ];
 
 const App = () => {
+  const [colorScheme, setColorScheme] = useState<ColorScheme>('light');
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
   return (
-    <AppShell header={<HeaderSimple links={navLinks} />}>
-      <main>
-        <Container>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="/users" element={<UserListPage />} />
-            <Route path="/contact" element={<ContactPage />} />
-            <Route path="*" element={<ErrorPage />} />
-          </Routes>
-        </Container>
-      </main>
-    </AppShell>
+    <ColorSchemeProvider
+      colorScheme={colorScheme}
+      toggleColorScheme={toggleColorScheme}
+    >
+      <MantineProvider
+        theme={{ colorScheme }}
+        withGlobalStyles
+        withNormalizeCSS
+      >
+        <BrowserRouter>
+          <AppShell header={<HeaderSimple links={navLinks} />}>
+            <main>
+              <Container>
+                <Routes>
+                  <Route index element={<HomePage />} />
+                  <Route path="/about" element={<AboutPage />} />
+                  <Route path="/users" element={<UserListPage />} />
+                  <Route path="/users/:id" element={<UserDetailPage />} />
+                  <Route path="/contact" element={<ContactPage />} />
+                  <Route path="*" element={<ErrorPage />} />
+                </Routes>
+              </Container>
+            </main>
+          </AppShell>
+        </BrowserRouter>
+      </MantineProvider>
+    </ColorSchemeProvider>
   );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,12 @@
-import { MantineProvider } from '@mantine/core';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const rootElement = document.getElementById('root') as HTMLElement;
+const root = ReactDOM.createRoot(rootElement);
+root.render(
   <React.StrictMode>
-    <MantineProvider withGlobalStyles withNormalizeCSS>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </MantineProvider>
+    <App />
   </React.StrictMode>
 );
