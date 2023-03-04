18fc1388d6e77d710acd0af15687854cce693984
Keitoto <keitasekihara@gmail.com>
2023-03-04 18:28:44 +0900

    ✨ Added about page

--- src/pages/About/AboutPage.tsx
+++ src/pages/About/AboutPage.tsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import { Group, Image, Stack, Text, Title } from '@mantine/core';
+import React from 'react';
 
 export const AboutPage = () => {
   return (
-    <div>AboutPage</div>
-  )
-}
+    <div>
+      <Title order={1} mb="xl" mt="xl">
+        About
+      </Title>
+      <Group align="top" noWrap grow spacing="md">
+        <Image src="https://images.unsplash.com/photo-1673378165516-35873c36f9e2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80" />
+        <Stack justify="flex-start" spacing="xl">
+          <Text size="md">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero porro
+            error, quos nostrum quas officia? Blanditiis, nostrum minus autem
+            sunt provident molestiae similique quam, praesentium sit excepturi
+            tempore expedita exercitationem!
+          </Text>
+          <Text size="lg">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero porro
+            error, quos nostrum quas officia? Blanditiis, nostrum minus autem
+            sunt provident molestiae similique quam, praesentium sit excepturi
+            tempore expedita exercitationem!
+          </Text>
+          <Text size="lg">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero porro
+            error, quos nostrum quas officia? Blanditiis, nostrum minus autem
+            sunt provident molestiae similique quam, praesentium sit excepturi
+            tempore expedita exercitationem!
+          </Text>
+        </Stack>
+      </Group>
+    </div>
+  );
+};
