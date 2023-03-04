5611ccfbe9883c127d124d20a8c64c881511062c
Keitoto <keitasekihara@gmail.com>
2023-03-04 19:23:40 +0900

    ✨ Added validation schema

---
+++ src/pages/Contact/validation.ts
@@ -0,0 +1,9 @@
+import * as yup from 'yup';
+
+export const contactSchema = yup.object({
+  fullname: yup.string().required(),
+  email: yup.string().email().required(),
+  message: yup.string().required(),
+  phone: yup.string().required(),
+  website: yup.string(),
+});
